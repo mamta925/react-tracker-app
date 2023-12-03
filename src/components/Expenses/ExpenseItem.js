77d6616e03c2ec028096d8ddd6579ec59da16041
mamta925 <mamtarajput925@gmail.com>
2023-12-03 20:15:14 +0530

    interview

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,19 @@
+import {useState} from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  let [title, setTitle] = useState(props.title)
   
   const clickHandle = ()=>{
-    alert("clicked")
+    setTitle("Changed")
   }
 
   return (
     <div className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
       <button onClick = {clickHandle}>change title</button>
